d5cd3b8a40f02aa8c75fd8110cf717ee0194f17a
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-10-09 14:48:42 +0200

    file setup

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
+  css: [
+    '@/assets/main.scss'
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/_colors.scss" as *;',
+        },
+      },
+    },
+  },
+})
