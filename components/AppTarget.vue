2f70f9e483e9f091ea525e84f72575c8d223fe3e
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-11-08 17:50:58 +0100

    fix: target section responsive

--- components/AppTarget.vue
+++ components/AppTarget.vue
@@ -1,21 +1,21 @@
 <template>
-    <section id="targets-section" class="container mt-32">
+    <section id="targets-section" class="container mt-32 mx-auto px-4">
         
         <h5>Target</h5>
         <hr>
 
-        <div class="flex justify-between targets-intro">
+        <div class="lg:flex justify-between targets-intro">
             <div class="basis-1/2">
-                <h2>Tutta la nostra esperienza nel marketing immobiliare a tua disposizione</h2>
+                <h2 class="text-[35px] lg:text-[40px]">Tutta la nostra esperienza nel marketing immobiliare a tua disposizione</h2>
             </div>
-            <div>
+            <div class="mt-7 lg:mt-0">
                 <a href="https://form.typeform.com/to/jd4BaOPX" target="_blank">
                     <button class="btn btn-main">Prenota una consulenza gratuita</button>
                 </a>
             </div>
         </div>
 
-        <div class="targets flex mt-6 gap-20">
+        <div class="targets lg:flex lg:mt-6 mt-16 gap-20">
             <div class="single-target basis-1/3">
                 <figure>
                     <img src="../assets/img/graphic-elements/target-element-1.webp" alt="">       
@@ -98,13 +98,10 @@
     }
 
     h2 {
-        font-size: 40px;
         font-weight: 700;
     }
 
     p {
-        font-size: 18px;
-        margin-top: 30px;
 
         span {
             color: #3DB8D8;
