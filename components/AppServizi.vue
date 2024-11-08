458d5a2be4b1f9322c58e3a7f1648b6bdaf0d574
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-11-08 10:07:19 +0100

    fix: services section responsive

--- components/AppServizi.vue
+++ components/AppServizi.vue
@@ -1,22 +1,22 @@
 <template>
-    <section id="services-section" class="container mt-40">
+    <section id="services-section" class="container mt-40 mx-auto px-4">
         
         <h5>Servizi</h5>
         <hr>
 
-        <div class="flex justify-between services-intro">
+        <div class="lg:flex justify-between services-intro">
             <div class="basis-1/2">
-                <h2>Ricevi notizie targetizzate alla vendita e crea il tuo team di professionisti</h2>
-                <p>Grazie a strategie mirate e personalizzate <span>in base alle tue esigenze</span> ti forniamo un asset completo per raggiungere i tuoi obiettivi</p>
+                <h2 class="text-[35px] lg:text-[40px]">Ricevi notizie targetizzate alla vendita e crea il tuo team di professionisti</h2>
+                <p class="text-[15px] lg:text-[18px]">Grazie a strategie mirate e personalizzate <span>in base alle tue esigenze</span> ti forniamo un asset completo per raggiungere i tuoi obiettivi</p>
             </div>
-            <div>
+            <div class="mt-7 lg:mt-0">
                 <a href="https://form.typeform.com/to/jd4BaOPX" target="_blank">
                     <button class="btn btn-main">Prenota una consulenza gratuita</button>
                 </a>
             </div>
         </div>
 
-        <div class="services flex mt-6 gap-20">
+        <div class="services lg:flex lg:mt-6 mt-16 gap-20">
             <div class="single-service service-1 basis-1/3">
                 <figure>
                     <img src="../assets/img/graphic-elements/services-element-1.webp" alt="">       
@@ -87,7 +87,7 @@
     }
 
     h5 {
-        font-size: 20px;
+
         font-weight: 700;
         color: #3DB8D8;
         margin-bottom: 10px;
@@ -99,12 +99,10 @@
     }
 
     h2 {
-        font-size: 40px;
         font-weight: 700;
     }
 
     p {
-        font-size: 18px;
         margin-top: 30px;
 
         span {
