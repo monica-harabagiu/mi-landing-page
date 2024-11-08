2ea86e5b98b5010d45838811ccada6f6ad0c4f19
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-11-08 17:59:54 +0100

    fix: ceo section responsive

--- components/AppCeo.vue
+++ components/AppCeo.vue
@@ -1,18 +1,18 @@
 <template>
-    <section id="ceo-section" class="mt-80">
-        <div class="container flex py-20">
+    <section id="ceo-section" class="mt-80 mx-auto px-4">
+        <div class="container lg:flex py-20">
             <div class="basis-1/2 flex justify-center relative">
                 <figure class="-mt-40">
                     <img src="../assets/img/gianluca-sgambati-ceo.webp" alt="" width="370">
                 </figure>
-                <div class="name-tag absolute">
+                <div class="name-tag absolute bottom-[-5%] right-[5%] lg:right-[15%]">
                     <h6>Gianluca Sgambati</h6>
                     <span>CEO & Founder</span>
                 </div>
             </div>
-            <div class="basis-1/2 right-half">
-                <h2>Duis in felis eu enim ullamcorper placerat sit amet in sapien</h2>
-                <p>
+            <div class="basis-1/2 right-half mt-20 lg:mt-0">
+                <h2 class="text-[35px] lg:text-[40px]">Duis in felis eu enim ullamcorper placerat sit amet in sapien</h2>
+                <p class="text-[15px] lg:text-[18px]">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis varius et tellus eget condimentum. Nam sit amet venenatis urna. Cras ut lacus quis quam aliquam interdum. Phasellus eu odio nibh. Donec enim ex, commodo scelerisque leo ultricies, luctus gravida diam. Fusce ut tortor vel libero vulputate imperdiet. Morbi nec volutpat ligula, a pretium diam. Vivamus pretium id purus vitae congue.
                 </p>
             </div>
@@ -59,8 +59,6 @@
             border-radius: 5px;
             padding: 5px 20px;
             transform: rotate(-15deg);
-            bottom: -5%;
-            right: 15%;
             
             h6 {
                 font-size: 20px;
