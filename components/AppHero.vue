768b2e7452ceed6d3479bfe08f17c6c6224d08ba
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-11-08 09:44:58 +0100

    fix: hero section responsive

--- components/AppHero.vue
+++ components/AppHero.vue
@@ -1,38 +1,38 @@
 <template>
     <section class="hero text-center py-40 flex flex-col gap-7 relative container">
-        <h1>
+        <h1 class="lg:text-[64px] text-[32px]">
             Scopri le strategie di <br>
             <span>Web Marketing Immobiliare</span>
         </h1>
-        <h6>
+        <h6 class="lg:text-[20px]">
             E diventa il n°1 della tua città!
         </h6>
         <a href="https://form.typeform.com/to/jd4BaOPX" target="_blank">
             <button class="btn btn-main">Ottieni una consulenza gratuita</button>
         </a>
 
-        <figure class="absolute element-1 top-5 left-0">
+        <figure class="hidden lg:block absolute element-1 top-5 left-0">
             <img src="../assets/img/graphic-elements/hero-element-1.webp" alt="" width="220">
         </figure>
-        <figure class="absolute element-2 top-24 right-0">
+        <figure class="hidden lg:block absolute element-2 top-24 right-0">
             <img src="../assets/img/graphic-elements/hero-element-2.webp" alt="" width="220">
         </figure>
-        <figure class="absolute element-3 bottom-0 right-72">
+        <figure class="hidden lg:block absolute element-3 bottom-0 right-72">
             <img src="../assets/img/graphic-elements/hero-element-3.webp" alt="" width="250">
         </figure>
-        <figure class="absolute element-4 bottom-32 left-[300px]">
+        <figure class="hidden lg:block absolute element-4 bottom-32 left-[300px]">
             <img src="../assets/img/graphic-elements/hero-element-4.webp" alt="" width="80">
         </figure>
-        <figure class="absolute element-5 bottom-32 right-32">
+        <figure class="hidden lg:block absolute element-5 bottom-32 right-32">
             <img src="../assets/img/graphic-elements/hero-element-5.webp" alt="" width="20">
         </figure>
-        <figure class="absolute element-6 bottom-0 left-40">
+        <figure class="hidden lg:block absolute element-6 bottom-0 left-40">
             <img src="../assets/img/graphic-elements/hero-element-6.webp" alt="" width="50">
         </figure>
-        <figure class="absolute element-7 bottom-52 left-32">
+        <figure class="hidden lg:block absolute element-7 bottom-52 left-32">
             <img src="../assets/img/graphic-elements/hero-element-7.webp" alt="" width="10">
         </figure>
-        <figure class="absolute element-8 bottom-60 right-60">
+        <figure class="hidden lg:block absolute element-8 bottom-60 right-60">
             <img src="../assets/img/graphic-elements/hero-element-8.webp" alt="" width="30">
         </figure>
     </section>
@@ -116,7 +116,6 @@
 
 <style scoped>
     h1 {
-        font-size: 64px;
         font-weight: 800;
 
         span {
@@ -124,18 +123,14 @@
         }
     }
 
-    h6 {
+    /* h6 {
         font-size: 20px;
-    }
+    } */
 
     a {
         font-size: 18px;
         font-weight: 500;
     }
 
-    @media only screen and (max-width: 950px) {
-        .absolute {
-            display: none;
-        }
-    }
+
 </style>
