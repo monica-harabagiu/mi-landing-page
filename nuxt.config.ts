aa69a5fb48eddf824876e873ebb4fb1635155acd
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-11-21 16:04:14 +0100

    fix: nuxt config tests

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
   css: [
     '@/assets/main.scss'
   ],
-  ssr: true,
-  nitro: {
-    preset: 'node-server'
+  ssr: false,
+  // nitro: {
+  //   baseURL: 'https://metodoimmobiliare.com/landing',
+  //   preset: 'node-server'
+  // },
+  // routeRules: {
+  //   "/": {
+  //     prerender: false
+  //   }
+  // },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
   },
   vite: {
     css: {
